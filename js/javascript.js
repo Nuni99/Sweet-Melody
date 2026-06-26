ce61ee370cd058b356b4f1b19df1e59d92764ba3
Nuni99 <arboledadiego969@gmail.com>
2026-06-26 08:22:31 -0500

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,156 @@
+
+//creacion de las ventanas emergentes
+
+const cancionesInfo = {
+    unhappy: {
+        titulo: "Unhappy",
+        imagen: "assets/img/clasificacion/unhappy.jpeg",
+        descripcion: "Una canción melancólica que transmite sentimientos de tristeza, vacío emocional y reflexión."
+    },
+
+    smoothCriminal: {
+        titulo: "Smooth Criminal",
+        imagen: "assets/img/clasificacion/smooth_criminal.jpg",
+        descripcion: "Un clásico de Michael Jackson cargado de energía, ritmo e intensidad."
+    },
+
+    breeze: {
+        titulo: "Breeze",
+        imagen: "assets/img/clasificacion/breeze.jpg",
+        descripcion: "Una composición emocional perteneciente al mod DDLC Fallen Angel que transmite nostalgia y melancolía."
+    },
+
+    impostor: {
+        titulo: "Impostor Syndrome",
+        imagen: "assets/img/clasificacion/impostor.jpeg",
+        descripcion: "Una canción que mezcla humor, inseguridad y reflexión personal."
+    },
+
+    bugambilia: {
+        titulo: "Bugambilia",
+        imagen: "assets/img/clasificacion/bugambilia.jpeg",
+        descripcion: "Una canción cargada de nostalgia y recuerdos. Su melodía suave y su interpretación evocan momentos pasados con una mezcla de tristeza y cariño."
+    },
+
+    untilIFoundYou: {
+        titulo: "Until I Found You",
+        imagen: "assets/img/clasificacion/until_i.jpeg",
+        descripcion: "Una pieza romántica que transmite esperanza y gratitud. Su estilo inspirado en los años 50 crea una sensación cálida y reconfortante."
+    },
+
+    ellaBailaSola: {
+        titulo: "Ella Baila Sola",
+        imagen: "assets/img/clasificacion/ellabailasola.jpeg",
+        descripcion: "Una canción que proyecta confianza y admiración. Su ritmo pegadizo y su energía relajada la convierten en una experiencia agradable y optimista."
+    },
+
+    youSayRun: {
+        titulo: "You Say Run",
+        imagen: "assets/img/clasificacion/yousayrun.jpeg",
+        descripcion: "Una composición instrumental reconocida por inspirar valentía y motivación. Su progresión creciente transmite la sensación de superar obstáculos."
+    },
+
+    fallenDown: {
+        titulo: "Fallen Down",
+        imagen: "assets/img/clasificacion/fallendown.jpeg",
+        descripcion: "Una melodía tranquila y emotiva que despierta sentimientos de nostalgia, paz y conexión emocional. Es una de las piezas más recordadas de Undertale."
+    },
+
+    mayonesa: {
+        titulo: "Mayonesa",
+        imagen: "assets/img/clasificacion/mayonesa.jpeg",
+        descripcion: "Una canción alegre y divertida que invita al movimiento y al buen humor. Su ritmo festivo la convierte en una fuente inmediata de energía positiva."
+    }
+};
+
+
+function mostrarCancion(id) {
+
+    const cancion = cancionesInfo[id];
+
+    document.getElementById("tituloModal").textContent =
+        cancion.titulo;
+
+    document.getElementById("imagenModal").src =
+        cancion.imagen;
+
+    document.getElementById("descripcionModal").textContent =
+        cancion.descripcion;
+
+    document.getElementById("modalCancion").style.display =
+        "flex";
+}
+
+function cerrarModal() {
+    document.getElementById("modalCancion").style.display =
+        "none";
+}
+
+
+
+// creacion de las tablas --> chamucolandia
+let canciones = JSON.parse(localStorage.getItem("canciones")) || [];
+
+function agregarCancion() {
+    const cancion = document.getElementById('nombre-cancion').value.trim();
+    const artista = document.getElementById('nombre-artista').value.trim();
+    const sentimiento = document.getElementById('eleccion-sentimiento').value;
+
+    if (!cancion || !artista || !sentimiento) {
+        alert('Por favor completa todos los campos.');
+        return;
+    }
+
+    canciones.push({ cancion, artista, sentimiento });
+    localStorage.setItem("canciones", JSON.stringify(canciones));
+
+    renderTabla();
+
+    document.getElementById('nombre-cancion').value = '';
+    document.getElementById('nombre-artista').value = '';
+    document.getElementById('eleccion-sentimiento').value = '';
+}
+
+function eliminarCancion(indice) {
+    canciones.splice(indice, 1);
+    localStorage.setItem("canciones", JSON.stringify(canciones));
+    renderTabla();
+}
+function renderTabla() {
+    const tbody = document.getElementById('tabla-body');
+    if (canciones.length === 0) {
+        tbody.innerHTML = `
+            <tr>
+                <td colspan="5"><tr><td colspan="5"><div class="vacio"><div class="simbolo_vacio">?</div><div><em>"La música es un mantra, debes dejarla fluir. No se trata de cuántas canciones puedas sacar, sino de cuánto corazón y alma pongas en cada una de ellas."<em></em><br><br>
+                                -Michael Jackson</div></div></td></tr></td>
+            </tr>
+        `;
+        return;
+    }
+
+    let filas = "";
+
+    canciones.forEach((cancion, indice) => {
+
+        filas += `
+            <tr>
+                <td style="color:var(--gris-texto)">${indice + 1}</td>
+                <td style="color:var(--gris-texto)">${cancion.cancion}</td>
+                <td style="color:var(--gris-texto)">${cancion.artista}</td>
+                <td style="color:var(--gris-texto)">${cancion.sentimiento}</td>
+                <td style="color:var(--gris-texto)">
+                    <button class="boton_eliminar" onclick="eliminarCancion(${indice})";>
+                        Eliminar
+                    </button>
+                </td>
+            </tr>
+        `;
+
+    });
+
+    tbody.innerHTML = filas;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderTabla();
+});
